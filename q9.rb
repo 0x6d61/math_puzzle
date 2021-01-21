a540b84f6b67fca4f2ed7b4a036d33df90e1cad1
0x6d61 <0x6d61@gmail.com>
2021-01-21 22:50:24 +0900

    q9追加

---
+++ q9.rb
@@ -0,0 +1,13 @@
+man, women = 21, 11
+list = Array.new(man * women) { 0 }
+list[0] = 1
+women.times { |w|
+  man.times { |m|
+    if (m != w) && (man - m != women - w)
+      list[m + man * w] += list[m - 1 + man * w] if m > 0
+      list[m + man * w] += list[m + man * (w - 1)] if w > 0
+    end
+  }
+}
+
+puts list[-2] + list[-man - 1]
